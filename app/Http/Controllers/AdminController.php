f24aadacd171ab6ceb019c77063e6c968daa7f63
TQThien32 <tranqthien3203@gmail.com>
2025-04-03 20:49:57 +0700

    BE dang ky

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,5 +7,15 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    
+    public function dangKy(Request $request)
+    {
+        Admin::create([
+            'email'                 =>  $request->email,
+            'password'              =>  bcrypt($request->password),
+        ]);
+        return response()->json([
+            'message'  =>   'Đăng ký tài khoản thành công.',
+            'status'   =>   true
+        ]);
+    }
 }
--- app/Http/Controllers/HocVienController.php
+++ app/Http/Controllers/HocVienController.php
@@ -7,5 +7,22 @@ use Illuminate\Http\Request;
 
 class HocVienController extends Controller
 {
-   
+    public function dangKy(Request $request)
+    {
+        HocVien::create([
+            'email' =>$request->email,
+            'password' => bcrypt($request->password),
+            'ho_ten' =>$request->ho_ten,
+            'ngay_sinh' =>$request->ngay_sinh,
+            'gioi_tinh' =>$request->gioi_tinh,
+            'so_cccd' =>$request->so_cccd,
+            'sdt' =>$request->sdt,
+            'dia_chi' =>$request->dia_chi,
+            'is_duyet' => 0,
+        ]);
+        return response()->json([
+            'message'  =>   'Vui lòng đợi duyệt tài khoản, mời bạn đang nhập lại sau.',
+            'status'   =>   true
+        ]);
+    }
 }
